04bfface23f47c79fcc6857d39040c308ce112ef
Mauricio Oran <mauricioperes46@gmail.com>
2023-06-26 15:56:06 -0400

    feat : things will be changed

---
+++ src/app/componentes/vagas/vagas.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VagasComponent } from './vagas.component';
+
+
+
+@NgModule({
+  declarations: [
+    VagasComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class VagasModule { }
